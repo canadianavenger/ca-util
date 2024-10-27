fdd890d6725ccd4b74ef6441d605d6bc6f1e7a4c
Canadian Avenger <174906062+canadianavenger@users.noreply.github.com>
2024-10-27 13:06:59 -0400

    Update memstream_getc.c
    BUGFIX: added return if input pointer is invalid with errno set to EBADF.

--- src/memstream/memstream_getc.c
+++ src/memstream/memstream_getc.c
@@ -2,6 +2,11 @@
 #include <errno.h>
 
 int memstream_getc(memstream_buf_t *buf) {
+    if(NULL == buf) {
+        errno = EBADF;
+        return -1;
+    }
+
     if(buf->pos >= buf->len) {
         errno = EIO;
         return EOF;
